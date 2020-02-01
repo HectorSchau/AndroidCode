6b6606917336fac04f69ffd96eed6b95e51d3bee
Hector <schauvinhold@gmail.com>
2020-02-01 00:35:17 -0300

    Proyecto vacio,listo para empezar to coding!
    Signed-off-by: Hector <schauvinhold@gmail.com>

--- app/src/main/java/com/example/santiago/MainActivity.java
+++ app/src/main/java/com/example/santiago/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    private void meimportaUnPingo{}
 }
 
-Private void meimportaUnPingo{}
+
