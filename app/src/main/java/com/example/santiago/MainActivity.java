435b2efc7f7633cf3b13870d7355c39ef6d64ed1
Hector <schauvinhold@gmail.com>
2020-02-04 08:57:34 -0300

    La pantalla principal va estando
    Signed-off-by: Hector <schauvinhold@gmail.com>

--- app/src/main/java/com/example/santiago/MainActivity.java
+++ app/src/main/java/com/example/santiago/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.santiago;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+//import android.support.v7.app.ActionBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    private void meimportaUnPingo(){};
+    private void meimportaUnPingo(){}
+    public void Telefonos(View view) {
+        Toast.makeText(getApplicationContext(), "Teléfonos de Utilidad Pública", Toast.LENGTH_SHORT).show();
+    }
+    public void Profesionales(View view) {
+        Toast.makeText(getApplicationContext(), "Profesionales...", Toast.LENGTH_SHORT).show();
+    }
 }
 
 
