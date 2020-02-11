7fd13e7214ed74b903bba2d4587fc744f6fecace
Hector <schauvinhold@gmail.com>
2020-02-11 16:58:35 -0300

    Empezamos con Profesionales
    Signed-off-by: Hector <schauvinhold@gmail.com>

--- app/src/main/java/com/example/santiago/MainActivity.java
+++ app/src/main/java/com/example/santiago/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.santiago;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
-//import android.support.v7.app.ActionBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
     }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
     private void meimportaUnPingo(){}
     public void Telefonos(View view) {
         Toast.makeText(getApplicationContext(), "Teléfonos de Utilidad Pública", Toast.LENGTH_SHORT).show();
     }
     public void Profesionales(View view) {
+        Intent intent = new Intent(this, Profesionales_Main_Activity.class);
+        startActivity(intent);
+
         Toast.makeText(getApplicationContext(), "Profesionales...", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/santiago/Profesionales_Main_Activity.java
@@ -0,0 +1,14 @@
+package com.example.santiago;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profesionales_Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profesionales__main_activity);
+    }
+}
